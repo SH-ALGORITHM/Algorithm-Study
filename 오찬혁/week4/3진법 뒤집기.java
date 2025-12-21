f35128b6331f264d94c73fb379ef5c9d8a544ce8
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-12-21 13:38:49 +0900

    feat: [오찬혁][251222] 3진법 뒤집기 제출

---
+++ 오찬혁/week4/3진법 뒤집기.java
@@ -0,0 +1,25 @@
+class Solution {
+	public int solution(int n) {
+		int result = 0;
+		String temp = "";
+
+		for(int i = n; i > 0; i/=3){
+			int j = i % 3;
+			temp += String.valueOf(j);
+		}
+
+		long num = Long.parseLong(temp);
+		int cnt = 0;
+
+		for(long i = num; i > 0; i/=10){
+			long j = i % 10;
+			for(long k = 0; k < cnt; k++){
+				j *= 3;
+			}
+			cnt++;
+			result += j;
+		}
+
+		return result;
+	}
+}
